11a4dedb81369d16963d9be2e51aac94bf75ad5d
Madhuri140492 <madhurideshmukh1404@gmail.com>
2022-11-23 17:31:41 +0530

    Second Largest Number in An Array

---
+++ SecondLargestElement.java
@@ -0,0 +1,21 @@
+package Day3_PracticeProblem;
+
+//Write a Java Program to Find 2nd Largest Number in an Array.
+
+public class SecondLargestElement {
+    public static void main(String[] args) {
+
+        int[] a={6,5,4,9,8,7,3,2,1};
+        int temp;
+        for (int i=0; i<a.length; i++){
+            for (int j=i+1; j<a.length; j++){
+                if (a[i]<a[j]){
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        System.out.println("Second Largest Number in Array is " +a[1]);
+    }
+}
